207de6abc5335aca8f24c7ce6ca7b4b9ccdbd851
Julio Haro <julioharocapetillo@gmail.com>
2024-06-20 19:48:25 -0600

    Proyecto 5

---
+++ Main.py
@@ -0,0 +1,72 @@
+import graphs as grafo
+from queue import Queue as que
+from graphs import Grafo as Grafo
+from dijkstra import dijkstra as dk
+from kruskalPrim import KruskalPrim as pk
+import random
+import spring as sprincito
+
+
+
+grafo = Grafo()
+        
+grafoMalla30 = Grafo().grafoBarabasiAlbert(500, 5, False)
+
+x=0
+for x in range(len(grafoMalla30.nodos)):
+    grafoMalla30.nodos[x].valorEquis = random.uniform(0,10)
+    print(grafoMalla30.nodos[x].valorEquis)
+    grafoMalla30.nodos[x].valorYe = random.uniform(0,10)
+    print(grafoMalla30.nodos[x].valorYe)
+    x+=1
+
+spring = sprincito
+
+spring.springDibujo(grafoMalla30)
+
+
+"""
+
+Testings de otras cosillas.
+
+krusk = pk.kruskal(grafoMalla30)
+
+grafo.generaGephi(krusk[0], "K-Chico")
+print("________________________________________")
+kruski = pk.kruskalInv(grafoMalla30)
+grafo.generaGephi(kruski[0], "KI-Chico")
+print("________________________________________")
+#prim = pk.prim(grafoMalla30)
+#print(prim)
+#grafo.generaGephi(prim[0],"prim-Chico")
+
+grafoMalla100 = Grafo().grafoBarabasiAlbert(100, 5, False)
+for arista in grafoMalla100.aristas:
+    arista.distancia = random.randint(0,10000)
+krusk = pk.kruskal(grafoMalla100)
+
+grafo.generaGephi(krusk[0], "K-Mediano")
+print("________________________________________")
+kruski = pk.kruskalInv(grafoMalla100)
+grafo.generaGephi(kruski[0], "KI-Mediano")
+print("________________________________________")
+#prim = pk.prim(grafoMalla100)
+#print(prim)
+#grafo.generaGephi(prim[0],"prim-Mediano")
+
+
+grafoMalla500 = Grafo().grafoBarabasiAlbert(500, 5, False)
+for arista in grafoMalla500.aristas:
+    arista.distancia = random.randint(0,10000)
+krusk = pk.kruskal(grafoMalla500)
+
+grafo.generaGephi(krusk[0], "K-Grande")
+print("________________________________________")
+kruski = pk.kruskalInv(grafoMalla500)
+grafo.generaGephi(kruski[0], "KI-Grande")
+print("________________________________________")
+#prim = pk.prim(grafoMalla500)
+#print(prim)
+#grafo.generaGephi(prim[0],"prim-Grande")
+
+"""
